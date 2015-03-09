da2a353edbb876d5b0039c48b44c45d67b55b557
rafal <rafalmularski@outlook.com>
2015-03-09 18:57:43 +0100

    Flight model

---
+++ db/migrate/20150309175241_create_flights.rb
@@ -0,0 +1,11 @@
+class CreateFlights < ActiveRecord::Migration
+  def change
+    create_table :flights do |t|
+      t.integer :from_airport_id
+      t.integer :to_airport_id
+      t.datetime :start
+      
+      t.timestamps null: false
+    end
+  end
+end
