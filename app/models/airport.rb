f81cfe815da9552ff7bb5676521e93aed8579a4e
rafal <rafalmularski@outlook.com>
2015-03-09 19:21:10 +0100

    Associations between Airport and Flight models

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,6 @@
 class Airport < ActiveRecord::Base
+  has_many :arriving_flights, class_name: "Flight", 
+                              foreign_key: "to_airport_id"
+  has_many :departing_flights, class_name: "Flight", 
+                               foreign_key: "from_airport_id"
 end
--- db/migrate/20150309175241_create_flights.rb
+++ db/migrate/20150309175241_create_flights.rb
@@ -4,7 +4,7 @@ class CreateFlights < ActiveRecord::Migration
       t.integer :from_airport_id
       t.integer :to_airport_id
       t.datetime :start
-      
+
       t.timestamps null: false
     end
   end
