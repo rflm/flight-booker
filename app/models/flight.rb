c6fd5d68a4d89e9ebe81d726d0a6e7ea89328266
rafal <rafalmularski@outlook.com>
2015-03-10 17:46:57 +0100

    Flight model tests

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ActiveRecord::Base
+  validates :from_airport, presence: true, format: { with: /\d/ }
+  validates :to_airport, presence: true, format: { with: /\d/ }
   belongs_to :from_airport, class_name: "Airport"
   belongs_to :to_airport, class_name: "Airport"
 end
