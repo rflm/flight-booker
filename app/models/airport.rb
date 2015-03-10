6de5a7cd24e3a728513acee93006ecc84aea1a06
rafal <rafalmularski@outlook.com>
2015-03-10 16:36:38 +0100

    Airport model tests

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ActiveRecord::Base
+  validates :code, presence: true, length: { is: 3 }, format: { with: /\A[A-Z]{3}\z/ }
+
   has_many :arriving_flights, class_name: "Flight", 
                               foreign_key: "to_airport_id"
   has_many :departing_flights, class_name: "Flight", 
---
+++ spec/factories/airports.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :airport do |a|
+    a.code "SFO"
+  end
+end
