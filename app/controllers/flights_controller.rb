cdcda5c5a7b702f4221f2303ca31730c99f0d9c1
rafal <rafalmularski@outlook.com>
2015-03-11 18:37:18 +0100

    Pick a Flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     @start_dates = Flight.dates
 
-    @flights = Flight.where({ from_airport_id: params[:from_id], to_airport_id: params[:to_id] }).sort
+    @flights = Flight.where({ from_airport_id: params[:from_id], to_airport_id: params[:to_id],
+                              start: params[:start_date].to_date.beginning_of_day..params[:start_date].to_date.end_of_day }).sort
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'flights#index'
+  get 'bookings/new' => 'bookings#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
